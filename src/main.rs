7e1c31910cb21d30b0034088b7547609c58c39a3
minoru-github <kurami0621@yahoo.co.jp>
2022-09-15 09:22:55 +0900

    refactor

--- src/main.rs
+++ src/main.rs
@@ -61,8 +61,15 @@ impl Sim {
         let mut rng: Mcg128Xsl64 = rand_pcg::Pcg64Mcg::new(890482);
 
         let mut state = State::new();
+        self.compute_score(&mut state);
+
         let mut best_state = state.clone();
-        while my_lib::time::update() < 0.3 {
+        loop {
+            let current_time = my_lib::time::update();
+            if current_time >= my_lib::time::LIMIT {
+                break;
+            }
+
             // 近傍探索
             state.change(&mut rng);
 
@@ -111,13 +118,48 @@ impl Input {
 }
 
 mod solver {
-    use super::State;
+    use super::*;
+
     pub fn mountain(best_state: &mut State, state: &mut State) {
         //! bese_state(self)を更新する。<br>
         //! 新しいStateのほうが悪いStateの場合は、stateをbest_stateに戻す。
 
         // 最小化の場合は > , 最大化の場合は < 。
         if best_state.score > state.score {
+            *best_state = state.clone();
+        } else {
+            *state = best_state.clone();
+        }
+    }
+
+    const T0: f64 = 2e3;
+    //const T1: f64 = 6e2; // 終端温度が高いと最後まで悪いスコアを許容する
+    const T1: f64 = 6e1; // 終端温度が高いと最後まで悪いスコアを許容する
+    pub fn sa(best_state: &mut State, state: &mut State, current_time: f64, rng: &mut Mcg128Xsl64) {
+        //! 焼きなまし法
+        //! https://scrapbox.io/minyorupgc/%E7%84%BC%E3%81%8D%E3%81%AA%E3%81%BE%E3%81%97%E6%B3%95
+
+        static mut T: f64 = T0;
+        static mut CNT: usize = 0;
+        let temperature = unsafe {
+            CNT += 1;
+            if CNT % 100 == 0 {
+                let t = current_time / my_lib::time::LIMIT;
+                T = T0.powf(1.0 - t) * T1.powf(t);
+            }
+            T
+        };
+
+        // 最大化の場合
+        let delta = (best_state.score as f64) - (state.score as f64);
+        // 最小化の場合
+        //let delta = (state.score as f64) - (best_state.score as f64);
+
+        let prob = f64::exp(-delta / temperature).min(1.0);
+
+        if delta < 0.0 {
+            *best_state = state.clone();
+        } else if rng.gen_bool(prob) {
             *best_state = state.clone();
         } else {
             *state = best_state.clone();
@@ -147,6 +189,8 @@ mod my_lib {
                 now
             }
         }
+
+        pub const LIMIT: f64 = 0.3;
     }
 
     #[derive(Debug, Clone, PartialEq)]
